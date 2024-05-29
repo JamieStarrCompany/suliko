7ba2e8687b83a0f8c645efde014a2f54c3d30112
JamieStarrr <61823154+JamieStarrCompany@users.noreply.github.com>
2024-05-29 21:41:09 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+$("#buttonA").on("click", function(event){
+    window.location.href= './A.html'
+});
+
+$("#buttonB").on("click", function(event){
+    window.location.href= './B.html'
+});
+
+$("#buttonC").on("click", function(event){
+    window.location.href= './C.html'
+});
